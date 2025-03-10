e8c419e75a6c2aaaec3b0924f261ec625cacd5c2
RaspberryTeam <tntu404.koroliuk@gmail.com>
2025-03-10 15:18:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,24 +28,28 @@ def set_webhook():
     else:
         return "Помилка при встановленні вебхука", 400
 
-# Обробник команди /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    
-    chat_id = message.chat.id
-    # markup = types.InlineKeyboardMarkup()
-    bot.send_message(chat_id, "Ласкаво просимо!")
-    # if get_token(chat_id) is None:
-    #     markup.add(types.InlineKeyboardButton('Вхід', callback_data='login'))
-    #     markup.add(types.InlineKeyboardButton('Реєстрація', callback_data='registration'))
-    # else:
-    #     if check_beehive_exists():
-    #         markup.add(types.InlineKeyboardButton('Переглянути пасіку', callback_data='view_apiary'))
-    #         markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
-    #     else:
-    #         markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
+    print("Отримано команду /start від:", message.chat.id)
+    bot.send_message(message.chat.id, "Ласкаво просимо!")
 
-    # bot.send_message(chat_id, "Ласкаво просимо!", reply_markup=markup)
+# @bot.message_handler(commands=['start'])
+# def send_welcome(message):
+    
+#     chat_id = message.chat.id
+#     markup = types.InlineKeyboardMarkup()
+#     bot.send_message(chat_id, "Ласкаво просимо!")
+#     if get_token(chat_id) is None:
+#         markup.add(types.InlineKeyboardButton('Вхід', callback_data='login'))
+#         markup.add(types.InlineKeyboardButton('Реєстрація', callback_data='registration'))
+#     else:
+#         if check_beehive_exists():
+#             markup.add(types.InlineKeyboardButton('Переглянути пасіку', callback_data='view_apiary'))
+#             markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
+#         else:
+#             markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
+
+#     bot.send_message(chat_id, "Ласкаво просимо!", reply_markup=markup)
 
 # Обробка callback-кнопок
 @bot.callback_query_handler(func=lambda call: call.data in ['login', 'registration', 'create_apiary', 'view_apiary', 'back_in_menu'])
