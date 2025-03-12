df94011f6225543178b433e3228ebd6207edf9c7
RaspberryTeam <tntu404.koroliuk@gmail.com>
2025-03-12 14:06:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ bot = TeleBot(TOKEN)
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    chat_id = message.chat.id
-    print(f"📬 Надсилаю привітальне повідомлення до чату {chat_id}")  # Логування
-    
-    try:
-        bot.send_message(chat_id, "Привіт! Це тестове повідомлення.")
-        print("✅ Повідомлення надіслано")
-    except Exception as e:
-        print(f"❌ Помилка при надсиланні повідомлення: {e}")
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
     print(f"🔑 BOT_TOKEN: {TOKEN}, webhook {WEBHOOK_URL}")  # Лог для перевірки
@@ -47,25 +36,25 @@ def webhook():
         return 'Internal Server Error', 500
 
 
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#     print(f"🚀 Отримано команду /start від {message.chat.id}")
-#     chat_id = message.chat.id
-#     markup = types.InlineKeyboardMarkup()
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    print(f"🚀 Отримано команду /start від {message.chat.id}")
+    chat_id = message.chat.id
+    markup = types.InlineKeyboardMarkup()
     
-#     bot.send_message(chat_id, "Ласкаво просимо!")
+    bot.send_message(chat_id, "Ласкаво просимо!")
 
-#     if get_token(chat_id) is None:
-#         markup.add(types.InlineKeyboardButton('Вхід', callback_data='login'))
-#         markup.add(types.InlineKeyboardButton('Реєстрація', callback_data='registration'))
-#     else:
-#         if check_beehive_exists(chat_id):  # Передаємо chat_id
-#             markup.add(types.InlineKeyboardButton('Переглянути пасіку', callback_data='view_apiary'))
-#             markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
-#         else:
-#             markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
+    if get_token(chat_id) is None:
+        markup.add(types.InlineKeyboardButton('Вхід', callback_data='login'))
+        markup.add(types.InlineKeyboardButton('Реєстрація', callback_data='registration'))
+    else:
+        if check_beehive_exists(chat_id):  # Передаємо chat_id
+            markup.add(types.InlineKeyboardButton('Переглянути пасіку', callback_data='view_apiary'))
+            markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
+        else:
+            markup.add(types.InlineKeyboardButton('Створити пасіку', callback_data='create_apiary'))
 
-#     bot.send_message(chat_id, "Виберіть опцію:", reply_markup=markup)
+    bot.send_message(chat_id, "Виберіть опцію:", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data in ['login', 'registration', 'create_apiary', 'view_apiary', 'back_in_menu'])
 def callback_handler(call):
